c173675e287e39d8c5f6a92cb916b66d9481e73f
enti <enti@enti.enti>
2022-02-18 14:19:14 +0100

    Terminando Juego cruzar carretera

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
 #!/usr/bin/node
 
+
 let http = require("http");
 let fs = require("fs");
 
 let http_server = http.createServer(function(req, res){
-	console.log(req.url);
 
 	var archivo = "index.html";
-		if (req.url != "/"){
-			
-			archivo = req.url.substring(1);
-			console.log(archivo);
-		}
-
+	if (req.url != "/"){
+		archivo = req.url.substring(1);
+	}
 
 	fs.readFile(archivo, function(err, data){
-		if(err){
-			console.log("Error");
-			return
-		}
-		res.writeHead(200);
+		if(err){ console.log("ERROR!"); return; }
 
-		res.end(data);
+	res.writeHead(200);
+
+	res.end(data);
+
 	});
 
 }).listen(1095);
